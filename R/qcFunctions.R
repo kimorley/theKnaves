31606658d4b2278386b104835b9c9a6ebddbc153
kimorley <km5@sanger.ac.uk>
2011-11-03 16:11:58 +0000

    Removed Loess function as is now incorporated into the adjustLRR function

--- R/qcFunctions.R
+++ R/qcFunctions.R
@@ -14,11 +14,3 @@ summaryLRR <- function(x){
 	sumLrr <- c(round(median(x[which(x > -2 & x <2)]),3), round(mean(x[which(x > -2 & x <2)]),3), round(sd(x[which(x > -2 & x <2)]),3), round(mad(x,na.rm=T)/1.4826,3)) # LRR is normal-ish but removed anyway  
 	return(sumLrr)
 } 
-
-adjLRR <- function(y){
-	adjData <- data.frame("pos"=seq(1,length(y)+100,1),"lrr"=c(rnorm(50,mean=median(y,na.rm=T),sd=(mad(y,na.rm=T)/1.4826)/4),y,rnorm(50,mean=median(y,na.rm=T),sd=(mad(y,na.rm=T)/1.4826)/4)))
-	adjData <- cbind(adjData,"weight"=(ifelse(abs(adjData$lrr) > 0.3,0,1)))  # Give weight of 0 to those observations with extreme values
-	fitModel <- loess(adjData$lrr ~ adjData$pos, span=0.1, weight=adjData$weight)
-	adjData <- cbind(adjData,"lrradj"=(adjData$lrr - predict(fitModel, data.frame(x=adjData$pos))))
-	return(adjData$lrradj[51:(length(y)+50)])
-}
