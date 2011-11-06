1d69fb8cccf89a7b42617f0857aa54366c539b93
kimorley <km5@sanger.ac.uk>
2011-11-06 20:07:00 +0000

    Modified to remove alpha value, which was causing plotting problems.

--- R/plotCluster.R
+++ R/plotCluster.R
@@ -1,16 +1,31 @@
-plotCluster <- function(data,model,save=F,print=T){
+plotCluster <- function(target,SNP,clusterDIR=getwd(),save=F,print=T){
+	if (missing(target)){
+		stop("Please supply target list object.")
+	}
+	if (missing(SNP)){
+		stop("Supply SNP for which clusters are to be plotted.")
+	}
+	if (file.exists(paste(clusterDIR,"/clusterFile-",SNP,".gzip",sep=""))){
+		loadCluster <- try(load(paste(clusterDIR,"/clusterFile-",SNP,".gzip",sep="")))
+		if (inherits(loadCluster, 'try-error')){
+			stop("Canonical cluster file for SNP ",SNP," does not exist in specified location.")
+		}
+	}else{
+		stop("Canonical cluster file for SNP ",SNP," does not exist in specified location.")
+	}
+	data <- target[names(target)==SNP][[1]]	
 	testData <- data.frame(t=seq(0,1,0.01))
-	testData <- cbind(testData,r=predict(model$model,testData))
+	testData <- cbind(testData,r=predict(clusterData$model,testData))
 	clusterPlot <- ggplot() + 
 			layer(
-					data = data, mapping = aes(x = t, y = r, colour = call, alpha = conf),
+					data = data, mapping = aes(x = t, y = r, colour = call),
 					geom = "point"
 			) + 
 			layer(
 					data = testData, mapping = aes(x = t, y = r),
 					geom = "line"
 			) +
-			scale_colour_brewer(palette="Set1") + ylab("R") + xlab(expression(theta))
+			scale_colour_brewer(palette="Set1") + ylab("R") + xlab(expression(theta)) + opts(title = SNP)
 	if (print){
 		print(clusterPlot)
 	}
