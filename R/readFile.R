57680cd97d9fafd6df1408217c81c4b885dd9f0e
kimorley <km5@sanger.ac.uk>
2011-11-06 20:07:30 +0000

    Now includes functions for reading in INTU and GTU information for single SNP from per-chromosome file.

--- R/readFile.R
+++ R/readFile.R
@@ -9,7 +9,7 @@ readIntensity <- function(FILENAME){
 	# Read and check
 	data <- read.table(FILENAME,h=T,colClasses="character",row.names=1)
 	if (sum(names(data)[1:2] == c("Coor", "Alleles")) != 2){
-		stop("Coor and Alleles in as headers in first two columns after SNP.")
+		stop("Coor and Alleles expected as headers in first two columns after SNP.")
 	}
 	map <- subset(data, select=c(Coor,Alleles))
 	intu <- data.frame(t(data[3:length(data)]))
@@ -30,6 +30,46 @@ readCallConf <- function(FILENAME,splitIDs=TRUE){
 	splitter <- function(x){
 		call <- substring(x,1,2)
 		conf <- substring(x,4,9)
+		callConf <- data.frame(call,as.numeric(as.character(conf)))
+		if (splitIDs){
+			ids <- sapply(row.names(temp),convertID)
+			row.names(callConf) <- ids
+			return(callConf)
+		}else{
+			row.names(callConf) <- row.names(temp)
+			return(callConf)
+		}
+	}
+	output <- lapply(temp,splitter)
+	return(output)
+}
+
+# [3] Read normalised intensity file for specific SNP (for canonical cluster generation)
+
+readSnpIntu <- function(SNP,clusterINTU){
+	cmd <- paste( "awk '{ if (NR==1 || $1 == \"" ,SNP, "\") print $0 }' " ,clusterINTU, sep="")
+	data <- read.table(pipe(cmd),h=T,row.names=1, colClasses="character")
+	if (sum(names(data)[1:2] == c("Coor", "Alleles")) != 2){
+		stop("Coor and Alleles expected as headers in first two columns after SNP.")
+	}
+	map <- subset(data, select=c(Coor,Alleles))
+	intu <- data.frame(t(data[3:length(data)]))
+	if (length(row.names(intu))%%2 == 1){
+		stop("Number of intensity values is not divisible by two; expecting two values (A and B) per sample.")
+	}
+	output <- list(map=map,intu=intu)
+	return(output)
+}
+
+# [4] Read genotype call/confidence for specific SNP (for canonical cluster generation)
+
+readSnpGtu <- function(SNP,clusterGTU,splitIDs=T){
+	cmd <- paste( "awk '{ if (NR==1 || $1 == \"" ,SNP, "\") print $0 }' " ,clusterGTU, sep="")
+	data <- read.table(pipe(cmd),h=T,row.names=1,colClasses="character")
+	temp <- data.frame(t(data))
+	splitter <- function(x){
+		call <- substring(x,1,2)
+		conf <- substring(x,4,9)
 		callConf <- data.frame(call,conf)
 		if (splitIDs){
 			ids <- sapply(row.names(temp),convertID)
@@ -43,3 +83,4 @@ readCallConf <- function(FILENAME,splitIDs=TRUE){
 	output <- lapply(temp,splitter)
 	return(output)
 }
+		
