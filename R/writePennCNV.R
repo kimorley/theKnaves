9bd153795a779ede2cf03dac3ad1ca0367bef8a4
kimorley <km5@sanger.ac.uk>
2011-11-21 12:57:23 +0000

    Split writing-out functions so that data files are written separately to input list files.

--- R/writePennCNV.R
+++ R/writePennCNV.R
@@ -1,9 +1,17 @@
 # Writes out the main input files for PennCNV with appropriate headers
-writePennCNVInd <- function(x, ID){
-	print("Function currently in development")
-#	outData <- subset(x,select=c(SNPName,BAF,LRRadj))
-#	names(outData) <- c("Name",paste(ID,"-",CHR,"A.B Allele Freq",sep=""),paste(ID,"-",CHR,"A.Log R Ratio",sep=""))
-#	write.table(outData,file=paste(ID,"-",CHR,".txt",sep=""),quote=F,row.names=F,sep = "\t")
+writePennCNVdata <- function(data, CHR, ID, outDIR=getwd()){
+	outData <- subset(data,select=c(SNP,BAF,waveLRR))
+	names(outData) <- c("Name",paste(ID,"-",CHR,"A.B Allele Freq",sep=""),paste(ID,"-",CHR,"A.Log R Ratio",sep=""))
+	write.table(outData,file=paste(outDIR,"/",ID,"-",CHR,".txt",sep=""),quote=F,row.names=F,sep = "\t")
+}
+
+writePennCNVlists <- function(trioIDs, CHR, outDIR=getwd()){
+	# Write trio list
+	list <- c(paste(outDIR,"/",trioIDs[3],"-",CHR,".txt",sep=""),paste(outDIR,"/",trioIDs[2],"-",CHR,".txt",sep=""),paste(outDIR,"/",trioIDs[1],"-",CHR,".txt",sep=""))
+	write.table(t(list),file=paste(outDIR,"/","famList-",CHR,".txt",sep=""),quote=F,row.names=F,col.names=F,sep = "\t")
+	# Write individual list
+	list <- c(paste(outDIR,"/",trioIDs[3],"-",CHR,".txt",sep=""),paste(outDIR,"/",trioIDs[2],"-",CHR,".txt",sep=""),paste(outDIR,"/",trioIDs[1],"-",CHR,".txt",sep=""))
+	write.table(list,file=paste(outDIR,"/","indList-",CHR,".txt",sep=""),quote=F,row.names=F,col.names=F,sep = "\t")
 }
 
 writePennCNVTrio <- function(trioData,outDir=getwd()){
@@ -18,7 +26,7 @@ writePennCNVTrio <- function(trioData,outDir=getwd()){
 	for (i in 1:3){
 		ID <- strsplit(as.character(unique(trioData[,i]$data$SampleID)),"_")[[1]][length(strsplit(as.character(unique(trioData[,i]$data$SampleID)),"_")[[1]])]
 		trioIDs <- c(trioIDs,ID)
-		outData <- subset(trioData[,i]$data,select=c(SNPName,BAF,LRR))
+		outData <- subset(trioData[,i]$data,select=c(SNP,BAF,waveLRR))
 		names(outData) <- c("Name",paste(ID,"A.B Allele Freq",sep=""),paste(ID,"A.Log R Ratio",sep=""))
 		write.table(outData,file=paste(outDir,"/",ID,".txt",sep=""),quote=F,row.names=F,sep = "\t")
 	}
